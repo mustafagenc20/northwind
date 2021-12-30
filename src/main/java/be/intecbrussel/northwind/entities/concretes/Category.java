e3b9e943f25106fa8763261676e501c1766da0c1
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-30 15:42:00 +0100

    initial commit

--- src/main/java/be/intecbrussel/northwind/entities/concretes/Category.java
+++ src/main/java/be/intecbrussel/northwind/entities/concretes/Category.java
@@ -1,5 +1,6 @@
 package be.intecbrussel.northwind.entities.concretes;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "categories")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "products"})
 public class Category {
 
     @Id
